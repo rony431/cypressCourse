72bae62697ea9ba5f3f87737c3f2faf060b1006d
Ronny <ronny@Ronnys-MacBook-Pro.local>
2023-02-16 20:38:30 -0500

    example api

--- cypress/e2e/testMartes.cy.js
+++ cypress/e2e/testMartes.cy.js
@@ -4,45 +4,27 @@ import selectorLogin from "../pages/selectorLogin"
 import selectorHome from "../pages/selectorHome"
 // Page Object Model
 // repositorio de objetos y selectores
+describe('POST /pet API', () => {
 
-describe('first test on tuesday', () => {
-  // before describe quiero visitar la url
-  // HOOKS 
-  var selector = selectorLogin().dataLogin
 
-  before ( function () {
-    cy.log('before solo')
-  })
-  beforeEach( function () {
-    cy.visit('')
-    cy.log('Before each test log')
-  })
-  after ( function (){
-    cy.log('Test finalizados')
-    cy.log(' it ha sido completado')
-    cy.get('#react-burger-menu-btn').click()
-    cy.get('#logout_sidebar_link').click()
-    // reporte 
-  })
-  afterEach ( function () {
-    cy.log('test afterEach')
-  })
- 
-  // NEGATIVE TEST
-  it('it has to validate negative test', () => {
-    cy.get(selector.selectorUser).type(selector.nameRandom())
-    cy.get(selector.selectorPassword).type(selector.numberRandom())
-    cy.get(selector.selectorButton).click()
-    cy.get(selector.selectorError).should('have.text',selector.messageError)
-    cy.get(selector.selectorError).contains(selector.messageErrorContain)
-  })
-   // HAPPY PATH
-   it('it has to validate website', () => {
-    cy.get(selector.selectorUser).type(selector.user)
-    cy.get(selector.selectorPassword).type(selector.password)
-    cy.get(selector.selectorButton).click()
-    cy.get(selector.productSelector).should('have.text','Products')
-    cy.get(selector.price).should('have.text', "$29.99")
-    cy.get(selector.price).contains("99")
-  })
-})
+  it('should return 200 when a new pet is successfully created', () => {
+    cy.request('POST', 'https://petstore.swagger.io/v2/pet/', {
+      id: 999,
+      name: 'joge',
+      category: {
+        id: 1,
+        name: 'Dogs',
+      },
+      photoUrls: ['photo_url'],
+      tags: [
+        {
+          id: 0,
+          name: 'dog',
+        },
+      ],
+      status: 'available',
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  });
+});
