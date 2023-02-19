4e00687bbd4cf24f5ee4d30087d515045cc702a6
Ronny <ronny@Ronnys-MacBook-Pro.local>
2023-02-19 13:03:31 -0500

    final sabado

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  video: false,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Inline Reporter',
+    embeddedScreenshots: true, 
+    inlineAssets: true, //Adds the asserts inline
+  },
   e2e: {
     chromeWebSecurity : false,
     baseUrl: 'https://www.saucedemo.com/',
     defaultCommandTimeout : 5000,
+    failOnStatusCode: false,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
 });
---
+++ cypress/pages/apiVariable.js
@@ -0,0 +1,16 @@
+export const pet = {
+    id: 431,
+    name: 'ronnypet',
+    category: {
+      id: 431,
+      name: 'ronnypet',
+    },
+    photoUrls: ['photo_url'],
+    tags: [
+      {
+        id: 0,
+        name: 'dog',
+      },
+    ],
+    status: 'available',
+  }
